49c1d4ee1e3bb58605413a50ba55cb2e42038f74
tui78787 <jaslyperezpun0suy@hotmail.com>
2024-05-07 08:52:47 +0700

    feat

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-function longestPalindrome(s) {
-  let longest = "";
-  for (let i = 0; i < s.length; i++) {
-    const oddPalindrome = expandAroundCenter(s, i, i);
-    const evenPalindrome = expandAroundCenter(s, i, i + 1);
-    const currentLongest =
-      oddPalindrome.length > evenPalindrome.length
-        ? oddPalindrome
-        : evenPalindrome;
-    if (currentLongest.length > longest.length) longest = currentLongest;
+function sortColors(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let i = 0;
+  while (i <= right) {
+    if (nums[i] === 0) {
+      [nums[i], nums[left]] = [nums[left], nums[i]];
+      left++;
+      i++;
+    } else if (nums[i] === 2) {
+      [nums[i], nums[right]] = [nums[right], nums[i]];
+      right--;
+    } else {
+      i++;
+    }
   }
-  return longest;
-}
-function expandAroundCenter(s, left, right) {
-  while (left >= 0 && right < s.length && s[left] === s[right]) {
-    left--;
-    right++;
-  }
-  return s.slice(left + 1, right);
 }
