adac4a030e5df899faa074b2c9f2e4700bfdab8c
tui78787 <jaslyperezpun0suy@hotmail.com>
2024-05-11 13:51:47 +0700

    init

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const numbers = [1, 5, 2, 8];
-const max = Math.max(...numbers);
-console.log(max); // 8
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
